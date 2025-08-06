c2e11d8c93c89390c2d14a30d52736b0c3ec9445
“yjl000” <623139771@qq.com>
2025-08-06 21:37:33 +0800

    feat: run model sucess, but has bug

--- agent.py
+++ agent.py
@@ -4,12 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import asyncio
+import os
 
-llm = ChatOpenAI(model="Deepseek")
+llm = ChatOpenAI(
+    model='deepseek-r1',
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+  )
 
 async def main():
     agent = Agent(
-        task="Compare the price of gpt-4o and DeepSeek-V3",
+        task="打开百度，搜索“百炼大模型”，不要分析和总结，直接运行搜索",
         llm=llm,
     )
     result = await agent.run()
